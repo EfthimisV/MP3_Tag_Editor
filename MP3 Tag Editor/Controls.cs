d810ed5bdbbb68db88226993825cd5ffe22860ec
Ευθύμης <programmernet744@outlook.com>
2018-03-17 11:35:41 +0200

    Add project files.

---
+++ MP3 Tag Editor/Controls.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace MP3_Tag_Editor
+{
+   
+    public class ExitButton : PictureBox
+    {
+        /// <summary> Δημιουργία του κομπιού για το κλείσιμο του παραθύρου </summary>
+        public ToolTip toolTip = new ToolTip();
+        public Form InstanceForm
+        {
+            get
+            {
+                return FindForm();
+            }
+        }
+        public ExitButton()
+        {
+            Size = new Size(23, 25);
+            SizeMode = PictureBoxSizeMode.StretchImage;
+            Image = Properties.Resources.CloseButtonStandard;
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            Image = Properties.Resources.CloseButtonPress;
+        }
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Image = Properties.Resources.CloseButtonOn;
+            toolTip.SetToolTip(this, "Έξοδος");
+        }
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            Image = Properties.Resources.CloseButtonStandard;
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            Image = Properties.Resources.CloseButtonOn;
+        }
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            InstanceForm.Close();
+        }
+    }
+    public class MinimizeButton : PictureBox
+    {
+        public ToolTip toolTip = new ToolTip();
+        public Form InstanceForm
+        {
+            get
+            {
+                return FindForm();
+            }
+        }
+        public MinimizeButton()
+        {
+            Size = new Size(23, 25);
+            SizeMode = PictureBoxSizeMode.StretchImage;
+            Image = Properties.Resources.MinimizeButtonStandard;
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            Image = Properties.Resources.MinimizeButtonPress;
+        }
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Image = Properties.Resources.MinimizeButtonOn;
+            toolTip.SetToolTip(this, "Ελαχιστοποίηση");
+        }
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            Image = Properties.Resources.MinimizeButtonStandard;
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            Image = Properties.Resources.MinimizeButtonOn;
+        }
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            InstanceForm.WindowState = FormWindowState.Minimized;
+        }
+    }
+    public class StatusBar : Panel
+    {
+        private int mousex, mousey;
+        private bool drag;
+
+        public Label ApplicationName = new Label();
+        public PictureBox Icon = new PictureBox();
+        public MinimizeButton MinButton = new MinimizeButton();
+        public ExitButton CloseButton = new ExitButton();
+        
+        [Description("The text associated with the control."), Category("Appearance")]
+        public string _ApplicationName
+        {
+            get
+            {
+                return ApplicationName.Text;
+            }
+            set
+            {
+                ApplicationName.Text = value;
+            }
+        }
+        [Description("Indicated the Icon for the form."), Category("Appearance")]
+        public Image _Icon
+        {
+            get
+            {
+                return Icon.Image;
+            }
+            set
+            {
+                Icon.Image = value;
+            }
+        }
+        public Form Instance
+        {
+            get
+            {
+                return FindForm();
+            }
+        }
+        
+        public StatusBar()
+        {
+            Dock = DockStyle.Top;
+            Height = 25;
+            BackColor = Color.FromArgb(11, 134, 196);
+            ApplicationName.AutoSize = true;
+            ApplicationName.ForeColor = Color.White;
+            ApplicationName.Font = new Font("Century Gothic", 10);
+            ApplicationName.Location = new Point(35, 2);
+            Icon.Size = new Size(18, 18);
+            Icon.SizeMode = PictureBoxSizeMode.StretchImage;
+            Icon.Location = new Point(10, 3);
+            MinButton.Location = new Point(978, 0);
+            CloseButton.Location = new Point(1000, 0);
+            Controls.Add(ApplicationName);
+            Controls.Add(Icon);
+            Controls.Add(MinButton);
+            Controls.Add(CloseButton);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            drag = true;
+            mousex = Cursor.Position.X - Instance.Left;
+            mousey = Cursor.Position.Y - Instance.Top;
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (drag)
+            {
+                Instance.Top = Cursor.Position.Y - mousey;
+                Instance.Left = Cursor.Position.X - mousex;
+            }
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            drag = false;
+        }
+    }
+    public class HamburgerMenu : Panel
+    {
+        public HamburgerMenu()
+        {
+            Dock = DockStyle.Left;
+        }
+    }
+}
---
+++ MP3 Tag Editor/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace MP3_Tag_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusBar1 = new MP3_Tag_Editor.StatusBar();
+            this.SuspendLayout();
+            // 
+            // statusBar1
+            // 
+            this.statusBar1._ApplicationName = "MP3 Tag Editor";
+            this.statusBar1._Icon = global::MP3_Tag_Editor.Properties.Resources.Icon;
+            this.statusBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(134)))), ((int)(((byte)(196)))));
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statusBar1.Location = new System.Drawing.Point(0, 0);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(1024, 25);
+            this.statusBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(1024, 680);
+            this.Controls.Add(this.statusBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MP3 Tag Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private StatusBar statusBar1;
+    }
+}
+
